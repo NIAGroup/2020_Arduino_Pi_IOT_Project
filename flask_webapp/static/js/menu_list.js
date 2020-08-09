8e7e7f13ff5d229475c89d9a5dd2afd50872aa2e
Adonay Berhe <adonay.berhe@intel.com>
2020-08-08 23:15:29 -0700

    Adittion of output and run sections along with scan icons

--- flask_webapp/static/js/menu_list.js
+++ flask_webapp/static/js/menu_list.js
@@ -1,4 +1,10 @@
 function openSelection(evt, selectionName) {
+  closeMenu();
+  document.getElementById(selectionName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-border-red";
+}
+
+function closeMenu(){
   var i, x, tablinks;
   x = document.getElementsByClassName("selection");
   for (i = 0; i < x.length; i++) {
@@ -8,10 +14,25 @@ function openSelection(evt, selectionName) {
   for (i = 0; i < x.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
   }
-  document.getElementById(selectionName).style.display = "block";
-  evt.currentTarget.firstElementChild.className += " w3-border-red";
 }
 
-function closeMenu(menu_id_name){
-document.getElementById(menu_id_name).style.display='none';
+function changeRunButtonColor(){
+  var btn;
+  btn = document.getElementById("runBtn");
+  
+  if(btn.classList.contains("btn-success"))
+  {
+    btn.classList.remove("btn-success");
+    btn.classList.add("btn-danger")
+  }
+  else if (btn.classList.contains("btn-danger"))
+  {
+    btn.classList.remove("btn-danger");
+    btn.classList.remove("btn-success");
+  }
+  else
+  {
+    btn.classList.add("btn-success");
+  }
+  
 }
