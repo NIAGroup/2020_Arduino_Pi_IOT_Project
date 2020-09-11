c82a0e46b3e445ca878ce6428b8b3bed9e29de84
James <felicia.james@intel.com>
2020-09-11 11:22:37 -0400

    updated html,css and js files

--- flask_webapp/static/js/menu_list.js
+++ flask_webapp/static/js/menu_list.js
@@ -36,3 +36,47 @@ function changeRunButtonColor(){
   }
   
 }
+
+function changeConnectButtonColor(){
+  var btn;
+  btn = document.getElementById("connectBtn");
+  
+  if(btn.classList.contains("btn-success"))
+  {
+    btn.classList.remove("btn-success");
+    btn.classList.add("btn-danger")
+  }
+  else if (btn.classList.contains("btn-danger"))
+  {
+    btn.classList.remove("btn-danger");
+    btn.classList.remove("btn-success");
+  }
+  else
+  {
+    btn.classList.add("btn-success");
+  }
+  
+}
+
+function thumbnailSelect(){
+  var btnContainer;
+  btnContainer = document.getElementById("thumbnailBtn");
+  
+  var btns;
+  btns = btnContainer.getElementsByClassName("thumbnail");
+  
+  // Loop through the buttons and add the active class to the current/clicked button
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+
+      // If there's no active class
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "");
+      }
+
+      // Add the active class to the current/clicked button
+      this.className += " active";
+    });
+	}
+}
