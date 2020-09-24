ad03173a848f401fd779acf5467b4441eb896943
nerdboyq <princtonb@gmail.com>
2020-09-24 15:49:04 -0700

    adding install bas script

--- arduino_pid_controller/code/btCommunication/BT_Communication_Standard.h
+++ arduino_pid_controller/code/btCommunication/BT_Communication_Standard.h
@@ -23,6 +23,28 @@ struct FullBtMsg{
  msg_byte overflow_byte; // byte 6
  msg_byte status_byte; // byte 7
 };
+
+struct SanityChecks {
+  const byte BT_echo = 0b1000;
+  const byte Servo_Pos = 0b1001;
+  const byte Sensor_Read = 0b1010;
+  const byte Tilt_Measure = 0b1011;
+};
+
+struct ServoCommand {
+ byte cmd;
+ byte pos; 
+};
+
+struct ServoLookupTbl {
+ const ServoCommand angle_90 = {0b0000,90};
+ const ServoCommand angle_60 = {0b0000,60};
+ const ServoCommand angle_30 = {0b0000,30};
+ const ServoCommand angle_0 = {0b0000,0};  // center position on servo
+ const ServoCommand angle_120 = {0b0000,120};
+ const ServoCommand angle_150 = {0b0000,150};
+ const ServoCommand angle_180 = {0b0000,180};
+};
     
 class BTComm_Standard {
   public:
