eab278c08c7e64e88519c5809a1b9536f8a3f178
Adonay Berhe <adonay.berhe@intel.com>
2020-10-11 17:07:13 -0400

    Adding the ability to return a scanned device list name to a reactive list

--- flask_webapp/app.py
+++ flask_webapp/app.py
@@ -1,11 +1,25 @@
 from flask import Flask, jsonify, request, redirect, render_template
-from src.device_list import BtDevContainer
+import sys
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index_new.html")
 
+@app.route("/scan")
+def scan():
+    devices = None
+    error = None
+
+    if sys.platform != 'win32':
+        from src.device_list import BtDevContainer
+        BtDevContainer()
+        devices = BtDevContainer.scan()
+    else:
+        error = "Running on Windows OS. Most likely won't have any devices discovered."
+        print(error)
+    return render_template("index_new_scan.html", devices=devices, error=error)
 
 if __name__ == '__main__':
     # setting the host to 0.0.0.0 makes the pi act as a server, 
@@ -13,4 +27,4 @@ if __name__ == '__main__':
     # local ip address. 
     # NOTE : When running the webapp you must use "sudo" for super user
     # rights to run as a server.
-    app.run(host="0.0.0.0",port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
