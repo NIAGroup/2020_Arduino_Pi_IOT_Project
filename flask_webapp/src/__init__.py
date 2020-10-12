f0122473d651c676e7b3da0b3ae8e846567707b2
Adonay Berhe <adonay.berhe@intel.com>
2020-10-12 14:44:57 -0400

    bug fix for parent directory init file

--- flask_webapp/src/__init__.py
+++ flask_webapp/src/__init__.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-DIR = os.path.dirname(os.path.abspath(__name__))
-WEB_HOME = os.pardir(DIR)
+DIR = os.path.abspath(os.path.dirname(__name__))
+WEB_HOME = os.path.abspath(os.path.join(DIR, os.pardir))
 
 sys.path.insert(0, DIR)
 sys.path.insert(1, WEB_HOME)
