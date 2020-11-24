0458b8181a180b82e5f191cee4bb5fde2ba18701
James <felicia.james@intel.com>
2020-11-23 21:35:16 -0500

    Updating branch

--- flask_webapp/app.py
+++ flask_webapp/app.py
@@ -3,12 +3,12 @@ from camera import VideoCamera
 import cv2
 import sys
 
-if sys.platform == 'win32':
-     print("Running on Windows OS. This is not supported yet.")
-     exit()
+# if sys.platform == 'win32':
+     # print("Running on Windows OS. This is not supported yet.")
+     # exit()
 
-from src.device_list import BtDevContainer
-Container = BtDevContainer()
+# from src.device_list import BtDevContainer
+# Container = BtDevContainer()
 
 app = Flask(__name__)
 
@@ -61,7 +61,7 @@ def connect():
             retValue[device] = False
     return jsonify(retValue)
     #print(devices)
-    #return jsonify({"test2": True, "test3": True})
+    return jsonify({"test2": True, "test3": True}) #uncommented line
 
 @app.route("/disconnect", methods=['GET', 'POST'])
 def disconnect():
