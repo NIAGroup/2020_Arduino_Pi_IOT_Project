1879084ba0f5de7297d507be0b3992c84870cdf9
Adonay Berhe <adonay.berhe@intel.com>
2020-11-30 20:37:23 -0500

    removing tester file

--- flask_webapp/src/tester.py
+++
@@ -1,7 +0,0 @@
-from device_list import *
-x = BtDevContainer()
-x.scan()
-x.get_device('Arduino_PID_0').connect()
-x.get_device('Arduino_PID_0').send_message('Sanity_Bt')
-
-import pdb; pdb.set_trace()
