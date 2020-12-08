1a9a2ed37e68cf777288c770a577a43cb05bf016
James <felicia.james@intel.com>
2020-12-07 20:46:33 -0500

    made changes to bluetooth section to incorperate two rows

--- flask_webapp/static/js/menu_list.js
+++ flask_webapp/static/js/menu_list.js
@@ -36,8 +36,8 @@ function clearChildNodes(elt_id){
   scan_dev_row.querySelectorAll('*').forEach(n => n.remove());
 }
 
-function drawNoDeviceMessage(){
-  var no_dev_text = document.createTextNode("Couldn't Find any bluetooth devices.");
+/* function drawNoDeviceMessage(){
+  var no_dev_text = document.createTextNode("Couldn't find any bluetooth devices.");
 
   var dev_header = document.createElement("h6");
   dev_header.style = "text-align:center; padding-top:15px";
@@ -50,8 +50,8 @@ function drawNoDeviceMessage(){
   var scan_dev_row = document.getElementById("scan_device_row");
   scan_dev_row.appendChild(dev_column);
 }
-
-function drawDevices(name, index){
+ */
+/* function drawDevices(name, index){
   var dev_image = document.createElement("img");
   dev_image.setAttribute("src", "../static/img/arduino_icon_transparent.jpg");
   dev_image.setAttribute("alt", "Arduino icon");
@@ -74,9 +74,9 @@ function drawDevices(name, index){
 
   var scan_dev_row = document.getElementById("scan_device_row");
   scan_dev_row.appendChild(dev_column);
-}
+} */
 
-function closeMenu(){
+/* function closeMenu(){
   var i, x, tablinks;
   x = document.getElementsByClassName("selection");
   for (i = 0; i < x.length; i++) {
@@ -86,7 +86,7 @@ function closeMenu(){
   for (i = 0; i < x.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
   }
-}
+} */
 
 function changeRunButtonColor(){
   var btn;
@@ -166,4 +166,89 @@ function triggerConnection(connectBtn){
 
 function thumbnailSelect(element){
   element.classList.toggle("active");
+}
+
+
+
+function closeMenu(){
+  var i, x; 
+  /* var tablinks; */
+  x = document.getElementsByClassName("selection");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+/*   tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+  } */
+}
+
+function scanBTDevice(evt, selectionName) {
+  if (document.getElementById(selectionName).style.display === "block"){
+    closeMenu();
+    return;
+  }
+  else{
+    closeMenu();
+	document.getElementById(selectionName).style.display = "block";
+    //evt.currentTarget.firstElementChild.className += "w3-border-red";
+     if (selectionName === "Scan"){
+      // Clear the row of previously scanned device image columns
+	  
+      setTimeout(function(){clearChildNodes("Scan");},10000);
+
+     // Perform a scan and grab data from back-end
+      var url = "/scan";
+      d3.json(url, {method: "GET", headers: {"Content-type": "application/json; charset=UTF-8"}}).then((data_in)=>{
+        if (data_in === null || data_in.scan_devs === undefined || data_in.scan_devs.length == 0) {
+          setTimeout(function(){drawNoDeviceMessage();},10000);
+        }
+        else{
+          data_in.scan_devs.forEach(drawDevices);
+        }
+      });
+    } 
+  }
+}
+
+function drawNoDeviceMessage(){
+  var no_dev_text = document.createTextNode("Couldn't find any bluetooth devices.");
+
+  var dev_header = document.createElement("h6");
+  dev_header.style = "text-align:center; padding-top:15px";
+  dev_header.appendChild(no_dev_text);
+
+  var dev_column = document.createElement("div");
+  dev_column.setAttribute("class", "col");
+  dev_column.appendChild(dev_header);
+
+  var scan_dev_row = document.getElementById("Scan");
+  scan_dev_row.appendChild(dev_column);
+}
+
+
+function drawDevices(name, index){
+  var dev_image = document.createElement("img");
+  dev_image.setAttribute("src", "../static/img/arduino_icon_transparent.jpg");
+  dev_image.setAttribute("alt", "Arduino icon");
+
+  var dev_a_tag = document.createElement("a");
+  dev_a_tag.setAttribute("class", "thumbnail");
+  dev_a_tag.setAttribute("onclick", "thumbnailSelect(this)");
+  dev_a_tag.appendChild(dev_image);
+
+  var dev_paragraph = document.createElement("p");
+  dev_paragraph.setAttribute("style", "text-align:center");
+  var dev_name_text = document.createTextNode(name);
+  dev_paragraph.appendChild(dev_name_text);
+
+  var dev_column = document.createElement("div");
+  dev_column.classList = "col scanDev";
+  dev_column.id = name;
+  dev_column.appendChild(dev_a_tag);
+  dev_column.appendChild(dev_paragraph);
+
+  var scan_dev_row = document.getElementById("scan_device_row");
+  scan_dev_row.appendChild(dev_column);
+
 }
